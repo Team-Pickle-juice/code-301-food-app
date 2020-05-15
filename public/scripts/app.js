6886eeb7bc1d977506297d357ee7f896f5106952
ticochuck <chucklivilla@gmail.com>
2020-05-14 22:02:05 -0700

    app.js working rendering price info for now.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,26 @@
 'use strict';
 
-$('.recipeInfo').on('click' , recipeInformation);
+$('.recipeForm').on('click' , recipeInformation);
 
 function recipeInformation (event) {
   event.preventDefault();
 
+  let API = 'abe8476cfcd04213ba99d65723e8028d';
+
   let requestedRecipe = $('.recipeID').val();
-  let url = `https://api.spoonacular.com/recipes/${requestedRecipe}/information?apiKey=${process.env.FOOD_API}&includeNutrition=true`
+  let url = `https://api.spoonacular.com/recipes/${requestedRecipe}/information?apiKey=${API}&includeNutrition=true`;
 
-  const ajaxSettings = {
-    method: 'get',
-    dataType: 'json',
-    data: {
-      recipe: ingredients,
-      recipe: instructions,
-      recipe: price,
-    },
-  };
+  // const ajaxSettings = {
+  //   method: 'get',
+  //   dataType: 'json',
+  //   data: {
+  //     // recipe: extendedIngredients,
+  //     // recipe: instructions,
+  //     recipe: pricePerServing,
+  //   },
+  // };
 
-  $.ajax('url', ajaxSettings)
+  $.ajax(url)
     .then( recipe => {
     renderRecipeInfo(recipe);
   });
@@ -28,5 +30,10 @@ function recipeInformation (event) {
 function renderRecipeInfo(data) {
   let template = $('#recipeInfo-template').html();
   let container = $('.recipeDetails');
-  container.html( Mustache.render(template, data) );
+  // let reee = {
+  //   price: data.pricePerServing
+  // };
+  let price = data.pricePerServing;
+console.log('This is the object:' + price);
+  container.html( Mustache.render(template, price) );
 }
